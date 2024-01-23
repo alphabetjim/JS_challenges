a83ae80e638c8182d0030023109cabb14bdf9cb9
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-23 09:52:37 +0000

    First exercise done

---
+++ ex1/index.js
@@ -0,0 +1,13 @@
+function js_style() {
+    // thePara = document.getElementById('text');
+    // thePara.style.fontSize = '6em';
+    // thePara.style.color = 'blue';
+    // thePara.style.fontFamily = 'arial';
+    text.style.fontSize = '14pt';
+    text.style.color = 'blue';
+    text.style.fontFamily = 'arial';
+
+    jsstyle.style.backgroundColor = 'orange';
+    jsstyle.style.color = 'white';
+    jsstyle.style.borderColor = 'pink';
+}
