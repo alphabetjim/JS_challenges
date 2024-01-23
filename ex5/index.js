6f915307d001cfb1e8dd5f43a689f44078e97e50
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-23 14:56:06 +0000

    Completed exercise 5

---
+++ ex5/index.js
@@ -0,0 +1,12 @@
+function insert_Row() {
+    let table = sampleTable;
+    let nrowsPresent = table.rows.length;
+    let row = document.createElement('tr');
+    for (let i=0; i<2; i++){
+        const cell = document.createElement("td");
+        const cellText = document.createTextNode(`Row${nrowsPresent+1}, cell${i+1}`);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+    }
+    table.children[0].appendChild(row);
+}
