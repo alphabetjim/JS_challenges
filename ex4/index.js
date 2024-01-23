064292fac8bf42beb285a539066aec6a49ff434a
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-23 14:16:44 +0000

    Added example solution in as an extra button

--- ex4/index.js
+++ ex4/index.js
@@ -1,6 +1,21 @@
+// my solution
 function getAttributes() {
     let attributes = w3r.attributes;
     for (attribute of attributes) {
         console.log(attribute);
     }
+}
+
+// given solution
+function getAttributesAsAlerts() {
+    let u = w3r.href;
+    alert(`The href is ${u}`);
+    let v = w3r.hreflang;
+    alert(`The hreflang is ${v}`);
+    let w = w3r.rel;
+    alert(`The rel is ${w}`);
+    let x = w3r.target;
+    alert(`The target is ${x}`);
+    let y = w3r.type;
+    alert(`The type is ${y}`);
 }
