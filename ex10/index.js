adcbb6918dbecbf074fd670d0a9f6d9b5b999764
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-24 10:38:41 +0000

    Inclusion of sign catching on user input

--- ex10/index.js
+++ ex10/index.js
@@ -1,5 +1,6 @@
 function sphereVolume(e) {
     e.preventDefault();
     let theRadius = myForm["radius"].value;
+    theRadius = Math.abs(theRadius);
     myForm["volume"].value = Math.PI*(4/3)*Math.pow(theRadius, 3);
 }
