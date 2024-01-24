b32563a9e32968e205c4510d12ca50127ab1694e
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-24 11:47:49 +0000

    Added functionality to remove highlight onmouseout

--- ex12/index.js
+++ ex12/index.js
@@ -1,9 +1,21 @@
 function highlightBold() {
+    // highlight bold elements within the first paragraph on the page
     let thePara = document.getElementsByTagName('p')[0];
     // loop through children setting background-color to yellow 
     for (child of thePara.children){
         if (child.tagName.toLowerCase() == 'b'){
             child.style.backgroundColor = "yellow";
+        }
+    }
+}
+
+function removeHighlight() {
+    // highlight bold elements within the first paragraph on the page
+    let thePara = document.getElementsByTagName('p')[0];
+    // loop through children setting background-color to yellow 
+    for (child of thePara.children){
+        if (child.tagName.toLowerCase() == 'b'){
+            child.style.backgroundColor = "white";
         }
     }
 }
