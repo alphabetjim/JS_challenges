038dc049788de1eaec5ef2090e046a9ef5c78e3f
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-24 09:41:48 +0000

    Solution permitting user input on button click

--- ex6/index.js
+++ ex6/index.js
@@ -1,10 +1,9 @@
-let row = 1;
-let column = 1;
-let cellString = "New cell text";
-
-// Initially not worrying about user input of values
 function changeContent() {
-    let theRow = myTable.rows[row-1];
-    let theCell = theRow.children[column-1];
+    // get values using JS user input
+    let row = window.prompt("Input the row number (0-2)", "0");
+    let column = window.prompt("Input the column number (0-1)", "0");
+    let cellString = window.prompt("Enter the cell text content");
+    let theRow = myTable.rows[parseInt(row,10)];
+    let theCell = theRow.children[parseInt(column,10)];
     theCell.innerHTML = cellString;
 }
