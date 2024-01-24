326f4a35e6352a21bad66d1bea2367e59bdb4b3a
James Bridgwater Court <james.bcourt@outlook.com>
2024-01-24 10:10:28 +0000

    Completed exercise 8

---
+++ ex8/index.js
@@ -0,0 +1,10 @@
+function removecolor() {
+    // get the colour that the user would like removed
+    let listItems = `Which colour would you like to remove? `
+    for (let i=0; i<colorSelect.children.length; i++) {
+        listItems += `${i} - ${colorSelect.children[i].innerHTML} `
+    }
+    let toRemove = window.prompt(listItems, '0');
+    toRemove = parseInt(toRemove, 10);
+    colorSelect.remove(toRemove);
+}
